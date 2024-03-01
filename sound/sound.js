d0ae68d8ea438152eb1aeb2756b5279360d83469
Schroeder Ryan <142847750+ThatWierdAnimator@users.noreply.github.com>
2024-03-01 22:45:41 +0000

    Fri, Mar 1, 2024, 2:45 PM -08:00

--- sound/sound.js
+++ sound/sound.js
@@ -3,7 +3,9 @@ sounds = ['dissapointed', 'foghorn']
 souns.forEach(sound => {
     btn = document.createElement('button')
     btn.classList.add('btn')
-    btn.innerText = sound
-
+    btn.innerText=sound
+    btn.addEventListener('click', () => {
+        
+    })
     document.getElementById('buttons').appendChild(btn)
 })
