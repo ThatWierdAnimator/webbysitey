ab4f70189ee48b5ad70819f5b0ead0567e8b8730
Schroeder Ryan <142847750+ThatWierdAnimator@users.noreply.github.com>
2024-03-15 21:47:03 +0000

    Fri, Mar 15, 2024, 2:47 PM -07:00

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 search = document.getElementById('search')
+loading = document.getElementById('loading')
+expanding = document.getElementById('expanding')
+sound = document.getElementById('sound')
+navigation = document.getElementById('navigation')
+landing = document.getElementById('landing')
 
 search.addEventListener('click', () => {
     window.open("search/search.html", "_self")
+})
+
+loading.addEventListener('click', () => {
+    window.open("loading/loading.html", "_self")
+})
+
+expanding.addEventListener('click', () => {
+    window.open("expanding/expanding.html", "_self")
+})
+
+sound.addEventListener('click', () => {
+    window.open("sound/sound.html", "_self")
+})
+
+navigation.addEventListener('click', () => {
+    window.open("navigation/navigation.html", "_self")
+})
+
+landing.addEventListener('click', () => {
+    window.open("landing/landing.html", "_self")
 })
